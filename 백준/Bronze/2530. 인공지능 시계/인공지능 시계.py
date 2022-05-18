89e1148f1cf00b4a3c79cf7aeaa02409dbee1755
Eonion <tkddjsdl33@hanmail.net>
2022-05-18 16:18:45 +0900

    Update 인공지능 시계.py

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -1,7 +1,26 @@
-h,m,s = map(int,input().split())
-t = int(input())
+# 처음풀이
+h, m, s = map(int, input().split())
+d = int(input())
 
-s += t
+m1 = (s + d) // 60
+s = (s + d) % 60
+
+m += m1
+
+if m >= 60:
+    h1 = m // 60
+    h += h1
+    m %= 60
+
+if h >= 24:
+    h %= 24
+print(h, m, s)
+
+# 더 쉽게 풀이
+h,m,s = map(int,input().split())
+d = int(input())
+
+s += d
 m += s//60
 h += m//60
 print(h%24,m%60,s%60)
