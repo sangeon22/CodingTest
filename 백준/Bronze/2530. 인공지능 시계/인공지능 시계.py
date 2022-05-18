02aaadf9873ec7fbb84d1414829ef6fa439433b9
Eonion <tkddjsdl33@hanmail.net>
2022-05-18 16:17:49 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -1,16 +1,7 @@
-h, m, s = map(int, input().split())
-d = int(input())
+h,m,s = map(int,input().split())
+t = int(input())
 
-m1 = (s + d) // 60
-s = (s + d) % 60
-
-m += m1
-
-if m >= 60:
-    h1 = m // 60
-    h += h1
-    m %= 60
-
-if h >= 24:
-    h %= 24
-print(h, m, s)
+s += t
+m += s//60
+h += m//60
+print(h%24,m%60,s%60)
