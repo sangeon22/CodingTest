64174817400512b79394ccf02081203bf4325988
Eonion <tkddjsdl33@hanmail.net>
2022-05-24 10:50:36 +0900

    [Bronze III] Title: 사분면, Time: 104 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.py
@@ -0,0 +1,30 @@
+n = int(input())
+q1 = 0
+q2 = 0
+q3 = 0
+q4 = 0
+axis = 0
+
+for i in range(n):
+    x, y = map(int, input().split())
+
+    if x > 0 and y > 0:
+        q1 += 1
+
+    elif x < 0 and y > 0:
+        q2 += 1
+
+    elif x < 0 and y < 0:
+        q3 += 1
+
+    elif x > 0 and y < 0:
+        q4 += 1
+
+    else:
+        axis += 1
+
+print("Q1:", q1)
+print("Q2:", q2)
+print("Q3:", q3)
+print("Q4:", q4)
+print("AXIS:", axis)
