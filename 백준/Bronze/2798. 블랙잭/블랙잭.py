efe16eec5952272ac962e32f2d27b8f0f1821598
Eonion <tkddjsdl33@hanmail.net>
2022-06-16 12:39:15 +0900

    [Bronze II] Title: 블랙잭, Time: 116 ms, Memory: 36188 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,13 +1,16 @@
 from sys import stdin
-
+from itertools import combinations
 n, m = map(int, stdin.readline().split())
 a = list(map(int, stdin.readline().split()))
 total = 0
 
-for i in range(0, len(a)):
-    for j in range(i+1, len(a)):
-        for k in range(j+1, len(a)):
-            sum_value = a[i] + a[j] + a[k]
-            if sum_value <= m:
-                total = max(total, sum_value)
-print(total)
+# for i in range(0, len(a)):
+#     for j in range(i + 1, len(a)):
+#         for k in range(j + 1, len(a)):
+#             sum_value = a[i] + a[j] + a[k]
+#             if sum_value <= m:
+#                 total = max(total, sum_value)
+# print(total)
+
+total = [sum(i) for i in combinations(a, 3) if sum(i) <= m]
+print(max(total))
