83f7e7618c69f999b231d3a93f80dd25c53dc298
Eonion <tkddjsdl33@hanmail.net>
2022-07-30 18:32:19 +0900

    [Bronze II] Title: 문자열 분석, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.py
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -1,20 +1,41 @@
-import sys
+from sys import stdin
 
 while True:
-    line = sys.stdin.readline().rstrip("\n")
-    low, up, num, space = 0, 0, 0, 0
+    # try:
 
-    if not line:
+    N = stdin.readline().rstrip("\n")
+    count = [0] * 4
+
+    # A, B, C, D = 0, 0, 0, 0
+    # for i in N:
+    #     if 'a' <= i <= 'z':
+    #         A += 1
+    #     elif 'A' <= i <= 'Z':
+    #         B += 1
+    #     elif i == ' ':
+    #         D += 1
+    #     else:
+    #         C += 1
+    # print(A, B, C, D)
+
+    if not N:
         break
 
-    for s in line:
-        if s.islower():
-            low += 1
-        elif s.isupper():
-            up += 1
-        elif s.isdigit():
-            num += 1
-        elif s.isspace():
-            space += 1
+    for i in N:
+        if i.islower():
+            count[0] += 1
+            # A += 1
+        elif i.isupper():
+            count[1] += 1
+            # B += 1
+        elif i.isdigit():
+            count[2] += 1
+            # D += 1
+        else:
+            count[3] += 1
+            # C += 1
+    print(*count)
+    # print(A, B, C, D)
 
-    print(low, up, num, space)
+
+
