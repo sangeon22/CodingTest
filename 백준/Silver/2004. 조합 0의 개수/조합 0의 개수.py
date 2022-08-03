386cb1b38c7071f7f6b4afe82c6fa1abef533376
Eonion <tkddjsdl33@hanmail.net>
2022-08-03 12:08:45 +0900

    [Silver II] Title: 조합 0의 개수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
@@ -1,22 +1,35 @@
 from sys import stdin
 
 
-def five(n):
+def f(x, y):
     answer = 0
-    while n != 0:
-        n = n // 5
-        answer += n
+    while x != 0:
+        x = x // y
+        answer += x
     return answer
 
 
-def two(n):
-    answer = 0
-    while n != 0:
-        n = n // 2
-        answer += n
-    return answer
+a, b = map(int, stdin.readline().split())
+
+print(min(f(a, 2) - f(a - b, 2) - f(b, 2), f(a, 5) - f(a - b, 5) - f(b, 5)))
 
 
-n, m = map(int, stdin.readline().split())
-
-print(min(two(n) - two(m) - two(n - m), five(n) - five(m) - five(n - m)))
+# def five(n):
+#     answer = 0
+#     while n != 0:
+#         n = n // 5
+#         answer += n
+#     return answer
+# 
+# 
+# def two(n):
+#     answer = 0
+#     while n != 0:
+#         n = n // 2
+#         answer += n
+#     return answer
+# 
+# 
+# n, m = map(int, stdin.readline().split())
+# 
+# print(min(two(n) - two(m) - two(n - m), five(n) - five(m) - five(n - m)))
