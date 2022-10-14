d03899834f2a1183eea850bc3057dbefdb8fac03
Eonion <tkddjsdl33@kakao.com>
2022-10-14 17:15:51 +0900

    [unrated] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.py
@@ -1,14 +1,18 @@
 def solution(rsp):
-    answer = ''
-    # 2 0
-    # 0 5
-    # 5 2
-    for i in rsp:
-        if i == '2':
-            answer += '0'
-        elif i == '0':
-            answer += '5'
-        elif i == '5':
-            answer += '2'
+#     answer = ''
+#     # 2 0
+#     # 0 5
+#     # 5 2
+#     for i in rsp:
+#         if i == '2':
+#             answer += '0'
+#         elif i == '0':
+#             answer += '5'
+#         elif i == '5':
+#             answer += '2'
         
-    return answer
+#     return answer
+
+    a = {'2':'0', '0':'5', '5':'2'}
+    
+    return ''.join( a[i] for i in rsp)
