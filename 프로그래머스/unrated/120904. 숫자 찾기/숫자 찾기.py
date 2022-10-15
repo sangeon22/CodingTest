9668b44d3202d7c4d4e7d43032f3bdf0ca588890
Eonion <tkddjsdl33@kakao.com>
2022-10-15 10:26:20 +0900

    [unrated] Title: 숫자 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.py
@@ -1,6 +1,8 @@
 def solution(num, k):
-    li = list(map(int, str(num)))
-    if k in li:
-        return li.index(k)+1
-    else:
-        return -1
+    # li = list(map(int, str(num)))
+    # if k in li:
+    #     return li.index(k)+1
+    # else:
+    #     return -1
+    a = str(num).find(str(k))+1
+    return a if a != 0 else -1
