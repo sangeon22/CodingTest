dc66312738718fa64fb726594f8aea5d4c442977
Eonion <tkddjsdl33@kakao.com>
2022-10-20 10:15:22 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,9 +1,10 @@
 def solution(numbers):
-    answer = 0
-    numbers.sort()
-    if numbers[0] * numbers[1] >= numbers[-1] * numbers[-2]:
-        return numbers[0] * numbers[1]
-    else:
-        return numbers[-1] * numbers[-2]
-
     # return sorted(numbers)[-1] * sorted(numbers)[-2]
+
+    # numbers.sort()
+    # if numbers[0] * numbers[1] >= numbers[-1] * numbers[-2]:
+    #     return numbers[0] * numbers[1]
+    # else:
+    #     return numbers[-1] * numbers[-2]
+
+    return max(sorted(numbers)[-1] * sorted(numbers)[-2], sorted(numbers)[0] * sorted(numbers)[1])
