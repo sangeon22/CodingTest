bd52e5747fca458a3e181b073f34acaa17e89c22
Eonion <tkddjsdl33@kakao.com>
2022-11-12 15:14:46 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,26 @@
+def solution(s):
+    answer = []
+    li = []
+
+    s = s.replace('{', '')
+    s = s.split("},")
+    s[-1] = s[-1].replace('}', '')
+
+    for i in s:
+        temp = i.split(',')
+        li.append(list(map(int, temp)))
+
+    li.sort(key=len)
+    
+    dic = {i:0 for i in li[-1]}
+    for i in li[-1]:
+        temp = 0
+        for j in li:
+            temp += j.count(i)
+        dic[i] = temp
+    
+    sorted_dic = sorted(dic.items(), key=lambda x:x[1], reverse =True)
+    for i in range(len(sorted_dic)):
+        answer.append(sorted_dic[i][0])
+        
+    return answer
