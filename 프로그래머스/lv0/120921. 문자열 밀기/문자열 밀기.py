558c3123131ed8ee9468f1de68d8ba8c7b646766
Eonion <tkddjsdl33@kakao.com>
2022-11-13 13:53:41 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -2,14 +2,29 @@ from collections import deque
 
 
 def solution(A, B):
-    answer = 0
-    a = deque(A)
+    
+#     첫 성공 코드
+#     answer = 0
+#     a = deque(list(A))
 
-    while (True):
-        if ''.join(i for i in a) == B:
-            return answer
-        elif answer == len(A):
-            return -1
+#     while (True):
+#         if ''.join(i for i in a) == B:
+#             return answer
+#         elif answer == len(A):
+#             return -1
+#         else:
+#             a.rotate(1)
+#             answer += 1
+
+# 코드 리팩토링
+    a = deque(A)
+    b = deque(B)
+
+    for i in range(len(A)):
+        if a == b:
+            return i
         else:
             a.rotate(1)
-            answer += 1
+            
+    return -1
+            
