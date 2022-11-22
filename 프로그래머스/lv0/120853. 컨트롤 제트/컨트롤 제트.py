e8fcaa490a7a2fbfdb5c20257899b5a67ae5de2e
Eonion <tkddjsdl33@kakao.com>
2022-11-22 09:58:29 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -2,7 +2,8 @@ def solution(s):
     s = s.split(" ")
     li = []
     
-    if s[0] != 'Z':
+    # if s[0] != 'Z':
+    if s[0].isnumeric:
         li.append(int(s[0]))
 
     for i in range(1, len(s)):
