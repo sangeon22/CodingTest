158a78934719c8693ee9a8e8c88697ec1330634f
Eonion <tkddjsdl33@kakao.com>
2022-11-23 10:22:16 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,19 +1,18 @@
 def solution(num, total):
     answer = []
     a = num // 2
-    mid = total / num
+    mid = int(total / num)
+    answer.append(mid)
 
     if total % num == 0:
-        answer.append(mid)
         for i in range(1, a + 1):
-            answer.append(int(mid) - i)
-            answer.append(int(mid) + i)
+            answer.append(mid - i)
+            answer.append(mid + i)
     else:
-        answer.append(int(mid))
-        answer.append(int(mid) + 1)
+        answer.append(mid + 1)
         for i in range(1, a):
-            answer.append(int(mid) - i)
-            answer.append(int(mid) + i+1)
+            answer.append(mid - i)
+            answer.append(mid + 1 + i)
 
     answer.sort()
     return answer
