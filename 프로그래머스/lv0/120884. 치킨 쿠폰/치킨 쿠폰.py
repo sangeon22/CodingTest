828e24685bb826725158f72d93b0d6f2a76d8141
Eonion <tkddjsdl33@kakao.com>
2022-11-25 10:12:40 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,10 @@
+def solution(chicken):
+    answer = 0
+    coupon = chicken
+
+    while coupon // 10 > 0:
+        answer += coupon // 10
+        coupon = (coupon // 10) + (coupon % 10)
+
+
+    return answer
