2e5dff61e942bb6ae6e4b7badad45cb20ed1762b
Eonion <tkddjsdl33@kakao.com>
2022-11-28 10:13:57 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -2,7 +2,16 @@ import math
 
 
 def solution(denum1, num1, denum2, num2):
-    lcm = num1 * num2 // math.gcd(num1, num2)
-    top = ((lcm // num1) * denum1) + ((lcm // num2) * denum2)
+    # 첫번째 풀이
+    # lcm = num1 * num2 // math.gcd(num1, num2)
+    # top = ((lcm // num1) * denum1) + ((lcm // num2) * denum2)
+    #
+    # return [top / math.gcd(top, lcm), lcm / math.gcd(top, lcm)]
 
-    return [top / math.gcd(top, lcm), lcm / math.gcd(top, lcm)]
+    # 두번째 풀이
+    # 2개 분수 합치기
+    denum = (denum1 * num2) + (denum2 * num1)
+    num = (num1 * num2)
+
+    # 최대공약수로 약분 -> 기약분수
+    return [denum // math.gcd(denum, num), num // math.gcd(denum, num)]
