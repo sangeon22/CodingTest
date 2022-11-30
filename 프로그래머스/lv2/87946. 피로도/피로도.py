8639eec3d76667d2f53677e73e5bbb3a67b5b49c
Eonion <tkddjsdl33@kakao.com>
2022-11-30 10:01:20 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -5,17 +5,18 @@ def solution(k, dungeons):
     answer = 0
     temp = 0
 
-    com = list(permutations(dungeons))
-    for i in com:
+    for i in permutations(dungeons):
         if temp > answer:
             answer = temp
+
+        if answer == len(dungeons):
+            return answer
+
         hp = k
         temp = 0
         for j in i:
             if hp >= j[0]:
                 hp -= j[1]
                 temp += 1
-        if answer == len(dungeons):
-            return answer
 
     return answer
