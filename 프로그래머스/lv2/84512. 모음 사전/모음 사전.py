a05c81e29f3de120411302cd7fc26e4867133efc
Eonion <tkddjsdl33@kakao.com>
2022-12-02 10:12:04 +0900

    [level 2] Title: 모음 사전, Time: 1.37 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -2,19 +2,20 @@ import itertools
 
 
 def solution(word):
-    answer = 0
-    l1 = ['A', 'E', 'I', 'O', 'U']
-    li1 = list(itertools.product(l1, repeat=1))
-    li2 = list(itertools.product(l1, repeat=2))
-    li3 = list(itertools.product(l1, repeat=3))
-    li4 = list(itertools.product(l1, repeat=4))
-    li5 = list(itertools.product(l1, repeat=5))
+    # l1 = ['A', 'E', 'I', 'O', 'U']
+    # li1 = list(itertools.product(l1, repeat=1))
+    # li2 = list(itertools.product(l1, repeat=2))
+    # li3 = list(itertools.product(l1, repeat=3))
+    # li4 = list(itertools.product(l1, repeat=4))
+    # li5 = list(itertools.product(l1, repeat=5))
+    #
+    # li = li1 + li2 + li3 + li4 + li5
+    #
+    # li.sort()
+    # count = 0
+    # for i in li:
+    #     count += 1
+    #     if "".join(i) == word:
+    #         return count
 
-    li = li1 + li2 + li3 + li4 + li5
-
-    li.sort()
-    count = 0
-    for i in li:
-        count += 1
-        if "".join(i) == word:
-            return count
+    return sorted(["".join(j) for i in range(1,6) for j in itertools.product(['A','E','I','O','U'], repeat=i)]).index(word)+1
