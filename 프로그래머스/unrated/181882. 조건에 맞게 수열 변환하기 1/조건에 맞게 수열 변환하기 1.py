118b8b89783c94da60a0a2dc708fc833b1d19cfd
Eonion <tkddjsdl33@kakao.com>
2023-10-19 22:13:57 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 10.15 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -1,10 +1,11 @@
 def solution(arr):
-    answer = []
-    for i in arr:
-        if i >= 50 and i % 2 == 0:
-            answer.append(i / 2)
-        elif i < 50 and i % 2 != 0:
-            answer.append(i * 2)
-        else:
-            answer.append(i)
-    return answer
+    # answer = []
+    # for i in arr:
+    #     if i >= 50 and i % 2 == 0:
+    #         answer.append(i / 2)
+    #     elif i < 50 and i % 2 != 0:
+    #         answer.append(i * 2)
+    #     else:
+    #         answer.append(i)
+    # return answer
+    return [i / 2 if i >= 50 and i % 2 == 0 else (i * 2 if i < 50 and i % 2 != 0 else i)for i in arr]
