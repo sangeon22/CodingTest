e1e8d4f7a7572cb6f2e87e28900a94d7ce8229d3
Eonion <tkddjsdl33@kakao.com>
2023-10-21 19:57:15 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,8 +1,9 @@
 def solution(num_list):
-    if num_list[-1] > num_list[-2]:
-        num_list.append(num_list[-1] - num_list[-2])
-        return num_list
-    else:
-        num_list.append(num_list[-1] * 2)
-        return num_list
-    
+#     if num_list[-1] > num_list[-2]:
+#         num_list.append(num_list[-1] - num_list[-2])
+#         return num_list
+#     else:
+#         num_list.append(num_list[-1] * 2)
+#         return num_list
+    num_list.append(num_list[-1] - num_list[-2]) if num_list[-1] > num_list[-2] else num_list.append(num_list[-1] * 2)
+    return num_list
