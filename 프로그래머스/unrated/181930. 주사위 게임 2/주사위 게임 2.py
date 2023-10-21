b7e314616e01b8313bc5b5b00d8bbd9741949e0e
Eonion <tkddjsdl33@kakao.com>
2023-10-21 19:34:20 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,8 @@
+def solution(a, b, c):
+    if len(list(set([a,b,c]))) == 3:
+        return a+b+c
+    elif len(list(set([a,b,c]))) == 2:
+        return (a + b + c) * (a**2 + b**2 + c**2)
+    elif len(list(set([a,b,c]))) == 1:  
+        return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+
