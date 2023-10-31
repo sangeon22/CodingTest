0ac1498c9c9d2bc01030d4614f9a841339b53eee
Eonion <tkddjsdl33@kakao.com>
2023-10-31 22:33:31 +0900

    [level unrated] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -1,2 +1,3 @@
 def solution(todo_list, finished):
-    return [i for i in dict(zip(todo_list,finished)) if not dict(zip(todo_list,finished)).get(i) ]
+    # return [i for i in dict(zip(todo_list,finished)) if not dict(zip(todo_list,finished)).get(i) ]
+    return [todo for idx, todo in enumerate(todo_list) if not finished[idx]]
