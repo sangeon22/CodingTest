5b26a11f6c9479f0beaf8313acb50f13354d91fa
Eonion <tkddjsdl33@kakao.com>
2023-11-01 22:03:48 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
@@ -1,11 +1,18 @@
 def solution(a, b):
-    if a % 2 == 0 :
-        if b % 2 == 0:
-            return abs(a-b)
-        else:
-            return (a+b) * 2
+    # if a % 2 == 0 :
+    #     if b % 2 == 0:
+    #         return abs(a-b)
+    #     else:
+    #         return (a+b) * 2
+    # else:
+    #     if b % 2 == 0:
+    #         return (a+b) * 2
+    #     else:
+    #         return a**2 + b**2
+    if a % 2 and b % 2:
+        return a**2 + b**2
+    elif a % 2 or b % 2:
+        return (a+b) * 2
     else:
-        if b % 2 == 0:
-            return (a+b) * 2
-        else:
-            return a**2 + b**2
+        return abs(a-b)
+        
