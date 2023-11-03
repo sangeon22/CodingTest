32027e82b88e16e4564fa7bccc55de53bf94953d
Eonion <tkddjsdl33@kakao.com>
2023-11-03 22:17:28 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,4 +1,5 @@
 def solution(num_list):
-    odd = sum(num_list[0::2])
-    even = sum(num_list[1::2])
-    return odd if even <= odd else even
+    # odd = sum(num_list[0::2])
+    # even = sum(num_list[1::2])
+    # return odd if even <= odd else even
+    return max(sum(num_list[0::2]), sum(num_list[1::2]))
