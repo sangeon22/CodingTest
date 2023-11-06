ec66e21efa4c7abfcf3cd8375c815ef891923ff7
Eonion <tkddjsdl33@kakao.com>
2023-11-06 22:06:43 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -1,12 +1,11 @@
 def solution(n):
     li = []
-    while True:
+    while n != 1:
         li.append(n)
         if n % 2 == 0:
             n /= 2
         else:
             n = 3 * n + 1
         
-        if n == 1:
-            li.append(n)
-            return li
+    li.append(n)
+    return li
