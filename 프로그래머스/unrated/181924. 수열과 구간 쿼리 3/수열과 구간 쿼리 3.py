cf0c2fa5c5981c431d9bdb0e797464c650504690
Eonion <tkddjsdl33@kakao.com>
2023-11-17 22:42:27 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,6 +1,10 @@
 def solution(arr, q):
-    for i in q:
-        a = arr[i[0]]
-        arr[i[0]] = arr[i[1]]
-        arr[i[1]] = a
-    return arr
+    # for i in q:
+    #     a = arr[i[0]]
+    #     arr[i[0]] = arr[i[1]]
+    #     arr[i[1]] = a
+    # return arr
+    
+    for i,j in q:
+        arr[i],arr[j] = arr[j],arr[i]
+    return arr        
