21bc2faf3da3853d4bede7dae7df833d6bc94973
Eonion <tkddjsdl33@kakao.com>
2023-11-23 22:33:19 +0900

    [level unrated] Title: 세 개의 구분자, Time: 11.78 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,15 +1,17 @@
 def solution(s):
     answer = []
-    temp = []
-    for i in s:
-        if i not in ['a', 'b', 'c']:
-            temp.append(i)
-        else:
-            answer.append(''.join(temp))
-            temp.clear()
-
-    answer.append(''.join(temp))
-    answer = list(filter(None, answer))
-    if len(answer) == 0:
-        return ["EMPTY"]
-    return answer
+    # temp = []
+    # for i in s:
+    #     if i not in ['a', 'b', 'c']:
+    #         temp.append(i)
+    #     else:
+    #         answer.append(''.join(temp))
+    #         temp.clear()
+    #
+    # answer.append(''.join(temp))
+    # answer = list(filter(None, answer))
+    # if len(answer) == 0:
+    #     return ["EMPTY"]
+    # return answer
+    answer = list(filter(None, s.replace('a', ' ').replace('b', ' ').replace('c', ' ').split(' ')))
+    return ['EMPTY'] if len(answer) == 0 else answer
