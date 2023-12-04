0138a325839bfc299a5f137b149c4a406dcd84f3
Eonion <tkddjsdl33@kakao.com>
2023-12-04 22:48:24 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -7,8 +7,5 @@ def solution(n, lost, reserve):
                 lost_set.remove(i - 1)
         elif i + 1 in lost_set:
                 lost_set.remove(i + 1)
-
-    if len(lost) == 1:
-        if lost[0] - 1 in reserve or lost[0] + 1 in reserve:
-            lost.remove(lost[0])            
+                
     return n - len(lost_set)
