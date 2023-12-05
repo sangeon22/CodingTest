e4c135785a615d9349f9c1430429139ea6ca2a46
Eonion <tkddjsdl33@kakao.com>
2023-12-05 22:00:52 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,11 +1,20 @@
 def solution(n, lost, reserve):
-    lost_set = set(lost) - set(reserve)
-    reserve_set = set(reserve) - set(lost)
+#     lost_set = set(lost) - set(reserve)
+#     reserve_set = set(reserve) - set(lost)
 
-    for i in reserve_set:
+#     for i in reserve_set:
+#         if i - 1 in lost_set:
+#                 lost_set.remove(i - 1)
+#         elif i + 1 in lost_set:
+#                 lost_set.remove(i + 1)
+#     return n - len(lost_set)
+
+    lost_set= list(set(lost) - set(reserve))
+    reserve_set = list(set(reserve) - set(lost))
+    
+    for i in reserve_set[:]:
         if i - 1 in lost_set:
-                lost_set.remove(i - 1)
+            lost_set.remove(i-1)
         elif i + 1 in lost_set:
-                lost_set.remove(i + 1)
-                
+            lost_set.remove(i+1)
     return n - len(lost_set)
