797355aa98c8620a15f0855fda4da5f1b24c00a2
Eonion <tkddjsdl33@kakao.com>
2024-02-01 21:25:15 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,17 @@
+import math
+
+
+def solution(n, a, b):
+    def binary(x, y, num):
+        if (x > num // 2 and y > num // 2) or (x <= num // 2 and y <= num // 2):
+            if abs(x - y) == 1:
+                return 1
+
+            if x - num // 2 < 1 or y - num // 2 < 1:
+                return binary(x, y, num // 2)
+            else:
+                return binary(x - num // 2, y - num // 2, num // 2)
+        else:
+            return int(math.log(num, 2))
+
+    return binary(a, b, n)
