d1443efc924a5d81f884dcfd617087d196f8fae8
Eonion <tkddjsdl33@kakao.com>
2024-02-22 22:34:45 +0900

    [level 1] Title: 대충 만든 자판, Time: 28.43 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,15 +1,18 @@
 def solution(keymap, targets):
     answer = []
+
     for i in targets:
         count = 0
         for j in range(len(i)):
+            # keymap의 각 요소에 target의 문자가 어느 인덱스에 위치하는지 찾음
+            # 없으면 102(keymap 최대 길이가 100이니까)
             idx = min(list(map(lambda x: x.index(i[j]) + 1 if i[j] in x else 102, keymap)))
             if idx >= 102:
                 answer.append(-1)
                 break
-
+                
             count += idx
-
+            
             if j == len(i) - 1:
                 answer.append(count)
     return answer
