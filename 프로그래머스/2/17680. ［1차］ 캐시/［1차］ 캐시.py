6d4e9709813b884e5e12f11f7db884570afca55b
Eonion <tkddjsdl33@kakao.com>
2024-03-20 15:16:55 +0900

    [level 2] Title: [1차] 캐시, Time: 1.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -14,17 +14,14 @@ def solution(cacheSize, cities):
         i = i.lower()
         
         # 캐시 내 등록 되어 있는 요소의 경우, cache hit 처리
-        # LRU 기법에 의해 가장 최근 참조 위치로 변경
-        if i in deq:
+        if i in deq: # LRU 기법에 의해 가장 최근 참조 위치로 변경
             answer += 1
             deq.remove(i)
-            deq.append(i)
-
-        # cache miss 처리
-        else:
+        else: # cache miss 처리
             answer += 5
-            deq.append(i)
 
+        deq.append(i)
+        
     return answer
 
 # cache hit: 1s
