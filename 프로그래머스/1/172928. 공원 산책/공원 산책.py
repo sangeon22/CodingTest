9fd74ef77d3401f01f5f822f991c05a1c3d75ef9
Eonion <tkddjsdl33@kakao.com>
2024-03-25 12:52:42 +0900

    [level 1] Title: 공원 산책, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,3 +1,4 @@
+# 주석 일부 깜빡해서 추가 후 재커밋
 def solution(park, routes):
     x, y = -1, -1
 
@@ -14,11 +15,16 @@ def solution(park, routes):
                 break
 
     for i in routes:
+        # 이동거리만큼 1씩 증가, 감소하다가 장애물(X)을 만나거나 맵(park)을 벗어나면 현재 좌표를 다시 원래 좔표로 돌리기 위한 원래좌표
         origin_x, origin_y = x, y
+        
+        # 방향, 이동 거리 추출
         item = i.split()
         direction = item[0]
         distance = int(item[1])
+        
         for _ in range(distance):
+            # 맵을 벗어나지 않고 장애물을 만나지 않는 경우, 방향으로 +-1칸 이동
             try:
                 if direction == "N" and 0 <= x - 1 <= park_h - 1 and park[x - 1][y] != 'X':
                     x -= 1
